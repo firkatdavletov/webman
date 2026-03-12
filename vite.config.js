858515aca79af52dbaa3cb14d6574c3b1ce8d660
firkatdavletov <dr.firkat@ya.ru>
2026-03-12 17:16:01 +0500

    fix url

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://api.aiymbrand.ru',
+                target: 'https://api.aiymbrand.ru',
                 changeOrigin: true,
             },
         },
